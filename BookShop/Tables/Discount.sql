8b121f72954ba9e8e5ab02fca4a771a5272e9d3c
Mikroas <llachesis.mutas@gmail.com>
2021-03-27 13:36:49 +0200

    Changed project type

---
+++ BookShop/Tables/Discount.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Discount]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [BookId] INT NOT NULL FOREIGN KEY REFERENCES [Book](Id), 
+    [DiscountSize] DECIMAL(5, 2) NOT NULL CHECK (00.00 <= DiscountSize AND DiscountSize <= 100.00), 
+    [Message] TEXT NOT NULL, 
+    [DateStart] DATE NOT NULL CHECK(DateStart < DateEnd), 
+    [DateEnd] DATE NOT NULL, 
+    
+)
