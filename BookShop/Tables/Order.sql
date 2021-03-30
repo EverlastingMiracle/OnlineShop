3978cf771a2d246ae7a553935ac25b1daef53c67
Mikroas <llachesis.mutas@gmail.com>
2021-03-30 20:58:46 +0300

    Edited tables

--- BookShop/Tables/Order.sql
+++ BookShop/Tables/Order.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Order]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [BookId] INT NOT NULL,
-    [Count] INT NOT NULL, 
-    [CustomerId] INT NOT NULL,
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    BookId INT NOT NULL,
+    Count INT NOT NULL, 
+    CustomerId INT NOT NULL,
 	CONSTRAINT FK_Order_to_Customer FOREIGN KEY (CustomerId) REFERENCES [Customer](Id), 
-	CONSTRAINt FK_Order_To_Book FOREIGN KEY (BookId) REFERENCES [Book](Id)
+	CONSTRAINt FK_Order_To_Book FOREIGN KEY (BookId) REFERENCES Book(Id)
 )
--- BookShop/Tables/Warehouse.sql
+++ BookShop/Tables/Warehouse.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Warehouse]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    	[BookId] INT NOT NULL,  
-    	[Count] INT NOT NULL,
-    	[LastUpdated] DATETIME NOT NULL,
-        CONSTRAINT FK_Warehouse_To_Book FOREIGN KEY (BookId) REFERENCES [Book](Id),
+	Id INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    BookId INT NOT NULL,  
+    Count INT NOT NULL,
+    LastUpdated DATETIME NOT NULL,
+    CONSTRAINT FK_Warehouse_To_Book FOREIGN KEY (BookId) REFERENCES Book(Id)
 )
